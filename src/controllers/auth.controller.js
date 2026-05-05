5cab8f3e0f0fcfea071d5102d4c1ed6962b531ee
Akbar Farooq <akbarfarooq2006@gmail.com>
2026-05-05 11:51:56 +0500

    feat: add jwt in regeister and getme api with jwt token to ensure authorization

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,13 @@
 import userModel from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../config/configs.js";
 
 // Register a new user
 export const register = async (req, res) => {
+
     const { username, email, password } = req.body;
 
-    // it will from database username or email is already exist or not
     const isUserAlreadyExist = await userModel.findOne({
         $or: [
             { username },
@@ -19,14 +21,47 @@ export const register = async (req, res) => {
 
     const hashpassword = await bcrypt.hash(password, 10);
 
-    
-
     const user = await userModel.create({
         username,
         email,
         password: hashpassword
     });
-    res.status(201).json({ success: true, message: "User registered successfully", user });
+
+    const token = jwt.sign(
+        { 
+            userId: user._id,
+            email: user.email,
+            username: user.username
+        },
+        config.JWT_SECRET,
+        { expiresIn: "7d" }
+    );
+
+    res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+        token,
+        User:{
+            id: user._id,
+            email: user.email,
+            username: user.username,
+        }
+     });
+}
+
+// Getme
+export const getMe = async (req, res) => {
+
+    const token = req.headers.authorization?.split(' ')[1];
+      
+    if (!token) {
+        return res.status(401).json({ message: "No token provided" });
+    }
+
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+    console.log("Decodded of JWT: ---------------> ",decoded);
+    res.status(200).json({ success: true, message: "User authenticated successfully", decoded });
+    
 
 }
 
@@ -46,24 +81,27 @@ export const register = async (req, res) => {
 
 
 
+// login user
+// export const login = async (req, res) => {
+//     console.log("Login Controller");
+// }
 
+// logout user
+// export const logout = async (req, res) => {
+//     console.log("Logout Controller");
+// }
 
+// logout all user
+// export const logoutAll = async (req, res) => {
+//     console.log("Logout All Controller");
+// }
 
-
-
-
-
-    // // login user
-    // export const login = async (req, res) => {
-    //     console.log("Login Controller");
-    // }
-
-    // // logout user
-    // export const logout = async (req, res) => {
-    //     console.log("Logout Controller");
-    // }
-
-    // // logout all user
-    // export const logoutAll = async (req, res) => {
-    //     console.log("Logout All Controller");
-    // }
+// delete all users
+export const deleteAllUsers = async (req, res) => {
+    try {
+        await userModel.deleteMany({});
+        res.status(200).json({ success: true, message: "All users deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { register} from "../controllers/auth.controller.js";
+import { register, deleteAllUsers, getMe } from "../controllers/auth.controller.js";
 
 
 const authRouter = Router();
 
 authRouter.post('/register', register);
+authRouter.get('/get-me', getMe);
+authRouter.delete('/delete', deleteAllUsers);
 
 export default authRouter;
